801e4c6bbca72d4648d4591c1173b4ed5dfa0989
tfpf <vpaijc@gmail.com>
2021-12-21 13:57:56 +0530

    Refactored (for readability) and optimised

--- solve_sudoku.c
+++ solve_sudoku.c
@@ -26,7 +26,6 @@ bool read_sudoku(char const *fname, int table[][9])
             char c;
             if(fscanf(fptr, " %c", &c) == 0 || (c != '-' && (c < '1' || c > '9')))
             {
-                printf("%c\n", c);
                 return false;
             }
 
@@ -107,8 +106,8 @@ void show(int table[][9])
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     row: int (a number from 1 to 9)
-//     num: int (a number from 1 to 9)
+//     row: int (a number from 0 to 8)
+//     num: int (a number from 0 to 8)
 //
 // Returns:
 //     bool (`true' if `num' may appear at row index `row', else `false')
@@ -132,8 +131,8 @@ bool allowed_in_row(int table[][9], int row, int num)
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     col: int (a number from 1 to 9)
-//     num: int (a number from 1 to 9)
+//     col: int (a number from 0 to 8)
+//     num: int (a number from 0 to 8)
 //
 // Returns:
 //     bool (`true' if `num' may appear at column index `col', else `false')
@@ -157,8 +156,8 @@ bool allowed_in_col(int table[][9], int col, int num)
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     row: int (a number from 1 to 9)
-//     col: int (a number from 1 to 9)
+//     row: int (a number from 0 to 8)
+//     col: int (a number from 0 to 8)
 //     num: int (a number from 1 to 9)
 //
 // Returns:
@@ -191,8 +190,8 @@ bool allowed_in_block(int table[][9], int row, int col, int num)
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     row: int (a number from 1 to 9)
-//     col: int (a number from 1 to 9)
+//     row: int (a number from 0 to 8)
+//     col: int (a number from 0 to 8)
 //     num: int (a number from 1 to 9)
 //
 // Returns:
@@ -215,11 +214,11 @@ bool allowed_at_position(int table[][9], int row, int col, int num)
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     row: int (a number from 1 to 9)
-//     col: int (a number from 1 to 9)
-//     random: bool (whether to assign `table[row][col]' randomly)
+//     row: int (a number from 0 to 8)
+//     col: int (a number from 0 to 8)
+//     assign_random: bool (whether to assign `table[row][col]' randomly)
 ///////////////////////////////////////////////////////////////////////////////
-void select_allowed(int table[][9], int row, int col, bool random)
+void select_allowed(int table[][9], int row, int col, bool assign_random)
 {
     int allowed;
     int count_allowed = 0;
@@ -232,7 +231,7 @@ void select_allowed(int table[][9], int row, int col, bool random)
         }
     }
 
-    if(count_allowed == 1 || (count_allowed >= 1 && random))
+    if(count_allowed == 1 || (count_allowed >= 1 && assign_random))
     {
         // GCC emits a superfluous warning for this line when compiling with
         // optimisations and warnings enabled.
@@ -240,6 +239,115 @@ void select_allowed(int table[][9], int row, int col, bool random)
         #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
         table[row][col] = allowed;
         #pragma GCC diagnostic pop
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Function
+// Check how many positions the given number can be placed at in the given row.
+// If there is only one position, assign it at there. Otherwise, do nothing.
+//
+// Args:
+//     table: int [][] (sudoku table)
+//     row: int (a number from 0 to 8)
+//     num: int (a number from 0 to 8)
+///////////////////////////////////////////////////////////////////////////////
+void select_possible_in_row(int table[][9], int row, int num)
+{
+    if(!allowed_in_row(table, row, num))
+    {
+        return;
+    }
+
+    int possible_col;
+    int count_possible = 0;
+    for(int j = 0; j < 9; ++j)
+    {
+        if(table[row][j] == 0 && allowed_in_col(table, j, num) && allowed_in_block(table, row, j, num))
+        {
+            possible_col = j;
+            ++count_possible;
+        }
+    }
+
+    if(count_possible == 1)
+    {
+        table[row][possible_col] = num;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Function
+// Check how many positions the given number can be placed at in the given
+// column. If there is only one position, assign it at there. Otherwise, do
+// nothing.
+//
+// Args:
+//     table: int [][] (sudoku table)
+//     col: int (a number from 0 to 8)
+//     num: int (a number from 0 to 8)
+///////////////////////////////////////////////////////////////////////////////
+void select_possible_in_col(int table[][9], int col, int num)
+{
+    if(!allowed_in_col(table, col, num))
+    {
+        return;
+    }
+
+    int possible_row;
+    int count_possible = 0;
+    for(int i = 0; i < 9; ++i)
+    {
+        if(table[i][col] == 0 && allowed_in_row(table, i, num) && allowed_in_block(table, i, col, num))
+        {
+            possible_row = i;
+            ++count_possible;
+        }
+    }
+
+    if(count_possible == 1)
+    {
+        table[possible_row][col] = num;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Function
+// Check how many positions the given number can be placed at in the given
+// block. If there is only one position, assign it at there. Otherwise, do
+// nothing.
+//
+// Args:
+//     table: int [][] (sudoku table)
+//     row: int (0, 3 or 6)
+//     col: int (0, 3 or 6)
+//     num: int (a number from 1 to 9)
+///////////////////////////////////////////////////////////////////////////////
+void select_possible_in_block(int table[][9], int row, int col, int num)
+{
+    if(!allowed_in_block(table, row, col, num))
+    {
+        return;
+    }
+
+    int possible_row, possible_col;
+    int count_possible = 0;
+    for(int i = row; i < row + 3; ++i)
+    {
+        for(int j = col; j < col + 3; ++j)
+        {
+            if(table[i][j] == 0 && allowed_at_position(table, i, j, num))
+            {
+                possible_row = i;
+                possible_col = j;
+                ++count_possible;
+            }
+        }
+    }
+
+    if(count_possible == 1)
+    {
+        table[possible_row][possible_col] = num;
     }
 }
 
@@ -255,87 +363,21 @@ void select_allowed(int table[][9], int row, int col, bool random)
 ///////////////////////////////////////////////////////////////////////////////
 void select_possible(int table[][9], int num)
 {
-    // Possibilities in each row.
     for(int i = 0; i < 9; ++i)
     {
-        if(!allowed_in_row(table, i, num))
-        {
-            continue;
-        }
-
-        int possible_row, possible_col;
-        int count_possible = 0;
-        for(int j = 0; j < 9; ++j)
-        {
-            if(table[i][j] == 0 && allowed_at_position(table, i, j, num))
-            {
-                possible_row = i;
-                possible_col = j;
-                ++count_possible;
-            }
-        }
-
-        if(count_possible == 1)
-        {
-            table[possible_row][possible_col] = num;
-        }
+        select_possible_in_row(table, i, num);
     }
 
-    // Possibilities in each column.
     for(int j = 0; j < 9; ++j)
     {
-        if(!allowed_in_col(table, j, num))
-        {
-            continue;
-        }
-
-        int possible_row, possible_col;
-        int count_possible = 0;
-        for(int i = 0; i < 9; ++i)
-        {
-            if(table[i][j] == 0 && allowed_at_position(table, i, j, num))
-            {
-                possible_row = i;
-                possible_col = j;
-                ++count_possible;
-            }
-        }
-
-        if(count_possible == 1)
-        {
-            table[possible_row][possible_col] = num;
-        }
+        select_possible_in_col(table, j, num);
     }
 
-    // Possibilities in each block.
-    for(int block_row_start = 0; block_row_start < 9; block_row_start += 3)
+    for(int i = 0; i < 9; i += 3)
     {
-        for(int block_col_start = 0; block_col_start < 9; block_col_start += 3)
+        for(int j = 0; j < 9; j += 3)
         {
-            if(!allowed_in_block(table, block_row_start, block_col_start, num))
-            {
-                continue;
-            }
-
-            int possible_row, possible_col;
-            int count_possible = 0;
-            for(int i = block_row_start; i < block_row_start + 3; ++i)
-            {
-                for(int j = block_col_start; j < block_col_start + 3; ++j)
-                {
-                    if(table[i][j] == 0 && allowed_at_position(table, i, j, num))
-                    {
-                        possible_row = i;
-                        possible_col = j;
-                        ++count_possible;
-                    }
-                }
-            }
-
-            if(count_possible == 1)
-            {
-                table[possible_row][possible_col] = num;
-            }
+            select_possible_in_block(table, i, j, num);
         }
     }
 }
@@ -346,9 +388,9 @@ void select_possible(int table[][9], int num)
 //
 // Args:
 //     table: int [][] (sudoku table)
-//     random: bool (whether to fill a cell randomly)
+//     assign_random: bool (whether to fill a cell randomly)
 ///////////////////////////////////////////////////////////////////////////////
-void single_pass(int table[][9], bool random)
+void single_pass(int table[][9], bool assign_random)
 {
     for(int i = 0; i < 9; ++i)
     {
@@ -356,9 +398,9 @@ void single_pass(int table[][9], bool random)
         {
             if(table[i][j] == 0)
             {
-                // Only one cell shall be filled randomly per function call.
-                select_allowed(table, i, j, random);
-                random = false;
+                // At most one cell shall be filled randomly per function call.
+                select_allowed(table, i, j, assign_random);
+                assign_random = false;
             }
         }
     }
@@ -389,9 +431,9 @@ void solve(int table[][9])
 
         // If no cells could be filled in an iteration, ask for a number to
         // be filled in randomly.
-        bool random = (zeros == prev_zeros) ? true : false;
+        bool assign_random = (zeros == prev_zeros) ? true : false;
 
-        single_pass(table, random);
+        single_pass(table, assign_random);
         prev_zeros = zeros;
     }
 }
