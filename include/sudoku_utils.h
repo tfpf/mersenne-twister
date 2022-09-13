e46c01a172cb55acfee81e3d72e6b6c5dbac2e7d
tfpf <19171016+tfpf@users.noreply.github.com>
2022-09-13 16:02:06 +0530

    Tried to separate `main` function.

---
+++ include/sudoku_utils.h
@@ -0,0 +1,21 @@
+#ifndef MERSENNE_TWISTER_SUDOKU_SOLVER_INCLUDE_SUDOKU_UTILS_H_
+#define MERSENNE_TWISTER_SUDOKU_SOLVER_INCLUDE_SUDOKU_UTILS_H_
+
+bool read_sudoku(char const *fname, int table[][9]);
+int number_of_empty_cells(int const table[][9]);
+void write_sudoku(int const table[][9]);
+bool allowed_in_row(int const table[][9], int row, int num);
+bool allowed_in_col(int const table[][9], int col, int num);
+bool allowed_in_block(int const table[][9], int row, int col, int num);
+bool allowed_at_position(int const table[][9], int row, int col, int num);
+void select_allowed(int table[][9], int row, int col, bool assign_random);
+void select_possible_in_row(int table[][9], int row, int num);
+void select_possible_in_col(int table[][9], int col, int num);
+void select_possible_in_block(int table[][9], int row, int col, int num);
+void select_possible(int table[][9], int num);
+void single_pass(int table[][9], bool assign_random);
+void solve_sudoku(int table[][9]);
+void generate_sudoku(int table[][9], double difficulty);
+bool validate_sudoku(int const table[][9], bool initial);
+
+#endif  // MERSENNE_TWISTER_SUDOKU_SOLVER_INCLUDE_SUDOKU_UTILS_H_
