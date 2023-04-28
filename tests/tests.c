946bd8af8311607b9ea065b8ee2aa111f98dc87e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-04-29 01:16:13 +0530

    Updated tests.
    Everything is tested together to check if any two objects interfere with each other.

--- tests/tests.c
+++ tests/tests.c
@@ -8,35 +8,45 @@
  * Test MT19937. The results of this implementation must match those of a C++
  * standard library implementation. See
  * https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine.
- * @{
  *****************************************************************************/
-void test_mt19937(void)
+void tests(void)
 {
     for(int i = 1; i < 10000; ++i)
     {
         mt19937_rand();
     }
-    uint32_t r = mt19937_rand();
-    assert(r == 0xF5CA0EDBU);
-}
-void test_mt19937_64(void)
-{
+
     for(int i = 1; i < 10000; ++i)
     {
         mt19937_64_rand();
     }
-    uint64_t r = mt19937_64_rand();
-    assert(r == 0x8A8592F5817ED872U);
+
+    struct mt19937_t mt32;
+    mt19937_seed(5489, &mt32);
+    for(int i = 1; i < 10000; ++i)
+    {
+        mt19937_rand(&mt32);
+    }
+
+    struct mt19937_64_t mt64;
+    mt19937_64_seed(5489, &mt64);
+    for(int i = 1; i < 10000; ++i)
+    {
+        mt19937_64_rand(&mt64);
+    }
+
+    // Placing all assertions here ensures that we detect if any two MT19937
+    // objects interfere with each other.
+    assert(mt19937_rand() == 0xF5CA0EDBU);
+    assert(mt19937_64_rand() == 0x8A8592F5817ED872U);
+    assert(mt19937_rand(&mt32) == 0xF5CA0EDBU);
+    assert(mt19937_64_rand(&mt64) == 0x8A8592F5817ED872U);
 }
-/******************************************************************************
- * @}
- *****************************************************************************/
 
 /******************************************************************************
  * Main function.
  *****************************************************************************/
 int main(void)
 {
-    test_mt19937();
-    test_mt19937_64();
+    tests();
 }
