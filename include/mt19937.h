013c3ccf66e4cc344ae0805cb9229e2263da27b5
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-04-29 23:15:32 +0530

    Macro definitions on a single line.
    Sometimes, warning messages display the first line of the macro. If this shows the user the full macro definition, it may become easier to debug.

--- include/mt19937.h
+++ include/mt19937.h
@@ -21,39 +21,30 @@ struct mt19937_64_t
 #define GET_OR_NULL(arg, ...) arg
 
 void mt19937_seed(uint32_t seed, struct mt19937_t *mt);
-#define mt19937_seed(seed, ...)  \
-mt19937_seed(seed, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_seed(seed, ...) mt19937_seed(seed, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 void mt19937_64_seed(uint64_t seed, struct mt19937_64_t *mt);
-#define mt19937_64_seed(seed, ...)  \
-mt19937_64_seed(seed, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_64_seed(seed, ...) mt19937_64_seed(seed, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 uint32_t mt19937_rand(struct mt19937_t *mt);
-#define mt19937_rand(...)  \
-mt19937_rand(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_rand(...) mt19937_rand(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 uint64_t mt19937_64_rand(struct mt19937_64_t *mt);
-#define mt19937_64_rand(...)  \
-mt19937_64_rand(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_64_rand(...) mt19937_64_rand(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 uint32_t mt19937_rand_integer(uint32_t modulus, struct mt19937_t *mt);
-#define mt19937_rand_integer(modulus, ...)  \
-mt19937_rand_integer(modulus, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_rand_integer(modulus, ...) mt19937_rand_integer(modulus, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 uint64_t mt19937_64_rand_integer(uint64_t modulus, struct mt19937_64_t *mt);
-#define mt19937_64_rand_integer(modulus, ...)  \
-mt19937_64_rand_integer(modulus, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_64_rand_integer(modulus, ...) mt19937_64_rand_integer(modulus, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 double mt19937_rand_real(struct mt19937_t *mt);
-#define mt19937_rand_real(...)  \
-mt19937_rand_real(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_rand_real(...) mt19937_rand_real(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 double long mt19937_64_rand_real(struct mt19937_64_t *mt);
-#define mt19937_64_rand_real(...)  \
-mt19937_64_rand_real(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_64_rand_real(...) mt19937_64_rand_real(GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 void mt19937_rand_shuffle(void *items, uint32_t num_of_items, size_t size_of_item, struct mt19937_t *mt);
-#define mt19937_rand_shuffle(items, num_of_items, size_of_item, ...)  \
-mt19937_rand_shuffle(items, num_of_items, size_of_item, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
+#define mt19937_rand_shuffle(items, num_of_items, size_of_item, ...) mt19937_rand_shuffle(items, num_of_items, size_of_item, GET_OR_NULL(__VA_ARGS__ __VA_OPT__(,) NULL))
 
 #endif  // TFPF_MERSENNE_TWISTER_INCLUDE_MT19937_H_
