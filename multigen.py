8a061b9ced2a7a2105b6c75ac435f4bc6770ed86
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-05-16 16:49:38 +0530

    Used a process pool.
    Instead of launching all process simultaneously.

--- multigen.py
+++ multigen.py
@@ -1,17 +1,32 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 """
 A script to generate multiple sudoku puzzles quickly.
+    python3 multigen.py [number of puzzles]
+This will create a directory 'Problems' and write the puzzles to files in it. By default, 10 puzzles will be generated.
 """
+
 
 import datetime
 import os
 import os.path
+import multiprocessing
 import subprocess
 import sys
 
 
+def create_puzzle(fname):
+    """
+Write a sudoku puzzle to a file, supressing standard error. Block until done.
+
+:param fname: File to write to.
+    """
+    with open(fname, 'w') as stdout, open(os.devnull, 'w') as stderr:
+       subprocess.call(('./solve_sudoku', '10.8'), stdout=stdout, stderr=stderr)
+
+
 def main():
+    """Main function."""
     if not os.access('solve_sudoku', os.X_OK):
         raise SystemExit('Compile the C program first.')
 
@@ -27,12 +42,14 @@ def main():
         if not os.path.isdir(directory):
             raise
 
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
     today = datetime.date.today()
     for i in range(num_of_puzzles):
         today += datetime.timedelta(days=1)
         fname = today.strftime(f'{directory}/S{i:02d}_%a-%d-%b-%Y.txt')
-        with open(fname, 'w') as stdout, open(os.devnull, 'w') as stderr:
-            subprocess.Popen(('./solve_sudoku', '10.8'), stdout=stdout, stderr=stderr)
+        pool.apply_async(create_puzzle, [fname])
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
