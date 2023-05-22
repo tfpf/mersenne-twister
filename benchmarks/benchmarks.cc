20e051621ec5e0f57c8d5683beffd3af9df8160c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-05-22 17:03:18 +0530

    Iterations as argument to macro.
    Number of iterations for seeder functions can be anything. That for a generator function should be a multiple of the length of the state array, so that the time taken to twist is not over-represented even by a minuscule amount.

--- benchmarks/benchmarks.cc
+++ benchmarks/benchmarks.cc
@@ -5,17 +5,16 @@
 // Neither GCC nor Clang eliminate the function call or loop while optimising.
 // One possible reason is that the definitions of the functions are available
 // only to the linker, not to the compiler.
-#define ITERATIONS 65536
-#define BENCHMARK(function_call)  \
+#define BENCHMARK(function_call, iterations)  \
 {  \
     auto begin = std::chrono::steady_clock::now();  \
-    for(int long i = 0; i < ITERATIONS; ++i)  \
+    for(int long i = 0; i < iterations; ++i)  \
     {  \
         function_call;  \
     }  \
     auto end = std::chrono::steady_clock::now();  \
     auto delay = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();  \
-    std::printf("%20s %8.1lf ns\n", #function_call, delay / static_cast<double>(ITERATIONS));  \
+    std::printf("%20s %8.1lf ns\n", #function_call, delay / static_cast<double>(iterations));  \
 }
 
 /******************************************************************************
@@ -23,10 +22,10 @@
  *****************************************************************************/
 int main(void)
 {
-    BENCHMARK(mt19937_seed32(0))
-    BENCHMARK(mt19937_seed64(0))
-    BENCHMARK(mt19937_rand32())
-    BENCHMARK(mt19937_rand64())
-    BENCHMARK(mt19937_real32())
-    BENCHMARK(mt19937_real64())
+    BENCHMARK(mt19937_seed32(0), 0x10000)
+    BENCHMARK(mt19937_seed64(0), 0x10000)
+    BENCHMARK(mt19937_rand32(), 0x270000)
+    BENCHMARK(mt19937_rand64(), 0x270000)
+    BENCHMARK(mt19937_real32(), 0x270000)
+    BENCHMARK(mt19937_real64(), 0x270000)
 }
