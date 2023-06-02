2db0718135be5ac9afa4f6671367ec6732c4128d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-06-02 16:12:06 +0530

    Sped up tests by not seeding again and again.

--- tests/C++/tests.cc
+++ tests/C++/tests.cc
@@ -20,31 +20,31 @@ void tests(void)
     assert(mt32.rand32() == 0xF5CA0EDBU);
     assert(mt64.rand64() == 0x8A8592F5817ED872U);
 
+    mt19937::init32();
     for(int i = 0; i < 30000; ++i)
     {
-        mt19937::init32();
+        std::uint32_t modulus = mt19937::rand32();
+        assert(mt19937::uint32(modulus) < modulus);
+        std::int32_t left = mt19937::rand32();
+        std::int32_t right = mt19937::rand32();
+        if(left < right)
         {
-            std::uint32_t modulus = mt19937::rand32();
-            assert(mt19937::uint32(modulus) < modulus);
-            std::int32_t left = mt19937::rand32();
-            std::int32_t right = mt19937::rand32();
-            if(left < right)
-            {
-                std::int32_t middle = mt19937::span32(left, right);
-                assert(left <= middle && middle < right);
-            }
+            std::int32_t middle = mt19937::span32(left, right);
+            assert(left <= middle && middle < right);
         }
-        mt19937::init64();
+    }
+
+    mt19937::init64();
+    for(int i = 0; i < 30000; ++i)
+    {
+        std::uint64_t modulus = mt19937::rand64();
+        assert(mt19937::uint64(modulus) < modulus);
+        std::int64_t left = mt19937::rand64();
+        std::int64_t right = mt19937::rand64();
+        if(left < right)
         {
-            std::uint64_t modulus = mt19937::rand64();
-            assert(mt19937::uint64(modulus) < modulus);
-            std::int64_t left = mt19937::rand64();
-            std::int64_t right = mt19937::rand64();
-            if(left < right)
-            {
-                std::int64_t middle = mt19937::span64(left, right);
-                assert(left <= middle && middle < right);
-            }
+            std::int64_t middle = mt19937::span64(left, right);
+            assert(left <= middle && middle < right);
         }
     }
 }
--- tests/C/tests.c
+++ tests/C/tests.c
@@ -22,31 +22,31 @@ void tests(void)
     assert(mt19937_rand32(&mt32) == 0xF5CA0EDBU);
     assert(mt19937_rand64(&mt64) == 0x8A8592F5817ED872U);
 
+    mt19937_init32(NULL);
     for(int i = 0; i < 30000; ++i)
     {
-        mt19937_init32(NULL);
+        uint32_t modulus = mt19937_rand32(NULL);
+        assert(mt19937_uint32(modulus, NULL) < modulus);
+        int32_t left = mt19937_rand32(NULL);
+        int32_t right = mt19937_rand32(NULL);
+        if(left < right)
         {
-            uint32_t modulus = mt19937_rand32(NULL);
-            assert(mt19937_uint32(modulus, NULL) < modulus);
-            int32_t left = mt19937_rand32(NULL);
-            int32_t right = mt19937_rand32(NULL);
-            if(left < right)
-            {
-                int32_t middle = mt19937_span32(left, right, NULL);
-                assert(left <= middle && middle < right);
-            }
+            int32_t middle = mt19937_span32(left, right, NULL);
+            assert(left <= middle && middle < right);
         }
-        mt19937_init64(NULL);
+    }
+
+    mt19937_init64(NULL);
+    for(int i = 0; i < 30000; ++i)
+    {
+        uint64_t modulus = mt19937_rand64(NULL);
+        assert(mt19937_uint64(modulus, NULL) < modulus);
+        int64_t left = mt19937_rand64(NULL);
+        int64_t right = mt19937_rand64(NULL);
+        if(left < right)
         {
-            uint64_t modulus = mt19937_rand64(NULL);
-            assert(mt19937_uint64(modulus, NULL) < modulus);
-            int64_t left = mt19937_rand64(NULL);
-            int64_t right = mt19937_rand64(NULL);
-            if(left < right)
-            {
-                int64_t middle = mt19937_span64(left, right, NULL);
-                assert(left <= middle && middle < right);
-            }
+            int64_t middle = mt19937_span64(left, right, NULL);
+            assert(left <= middle && middle < right);
         }
     }
 }
