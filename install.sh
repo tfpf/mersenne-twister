38134f98671a5b206c021774cca7a1c2818a5643
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-06-15 22:26:45 +0530

    Install script modified to work on MSYS2.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 target=/tmp/mersenne-twister-$(date +%s)
 git clone https://github.com/tfpf/mersenne-twister.git $target
 cd $target
-sudo make install
+case $(uname 2>/dev/null) in
+    MINGW* | MSYS*) make install;;
+    Linux) sudo make install;;
+esac
