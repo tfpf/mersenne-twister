36cc1d3b04b2092b2c655b496989a34466d26cd3
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-08-26 20:47:02 +0530

    Simplified Python installation files.
    It is working without some of the information. I assume it is not required.

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import Extension, find_packages, setup
 
 ext_modules = [Extension(
     name='mt19937',
-    sources=['./lib/pymt19937.c', './lib/mt19937.c'],
-    include_dirs=['./include'],
+    sources=['lib/pymt19937.c', 'lib/mt19937.c'],
+    include_dirs=['include'],
     extra_compile_args=['-O3'],
     py_limited_api=True,
 )]
 kwargs = dict(
-    packages=find_packages(where='./lib'),
-    package_dir={'': './lib'},
+    package_dir={'': 'lib'},
     ext_modules=ext_modules,
 )
 setup(**kwargs)
