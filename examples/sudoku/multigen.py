314aac5e74ab056c30c3a7a7fec661a73fceefb8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-07-26 22:32:20 +0530

    Generate puzzles every month instead of daily

--- examples/sudoku/multigen.py
+++ examples/sudoku/multigen.py
@@ -1,55 +1,26 @@
 #! /usr/bin/env python3
 
-"""
-A script to generate multiple sudoku puzzles quickly.
-    python3 multigen.py [number of puzzles]
-This will create a directory 'Problems' and write the puzzles to files in it. By default, 10 puzzles will be generated.
-"""
-
-
 import datetime
-import os
-import os.path
-import multiprocessing
 import subprocess
 import sys
 
-
-def create_puzzle(fname):
-    """
-Write a sudoku puzzle to a file, supressing standard error. Block until done.
-
-:param fname: File to write to.
-    """
-    with open(fname, 'w') as stdout, open(os.devnull, 'w') as stderr:
-       subprocess.call(('./sudoku', '11'), stdout=stdout, stderr=stderr)
-
-
 def main():
-    """Main function."""
-    if not os.access('sudoku', os.X_OK):
-        raise SystemExit('Compile the C program first.')
-
     try:
         num_of_puzzles = int(sys.argv[1])
     except (IndexError, ValueError):
-        num_of_puzzles = 10
+        num_of_puzzles = 31
 
-    directory = 'Problems'
-    try:
-        os.mkdir(directory)
-    except FileExistsError as e:
-        if not os.path.isdir(directory):
-            raise
-
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    today = datetime.date.today()
-    for i in range(num_of_puzzles):
-        today += datetime.timedelta(days=1)
-        fname = today.strftime(f'{directory}/S{i:02d}_%a-%d-%b-%Y.txt')
-        pool.apply_async(create_puzzle, [fname])
-    pool.close()
-    pool.join()
+    with open("index.html", "w", encoding="utf8") as writer:
+        print("<!DOCTYPE html><html><head><title>Sudoku</title></head><body>", file=writer)
+        date = datetime.date.today()
+        for _ in range(31):
+            print("<h1>", date.strftime("%A %d %B %Y"), "</h1>", file=writer, sep="")
+            for icon, difficulty in zip(["✍️", "✍️✍️"], ["11", "12"]):
+                print("<h2>", icon, "</h2><pre>", file=writer, flush=True, sep="")
+                subprocess.check_call(["./sudoku", difficulty], stdout=writer)
+                print("</pre>", file=writer)
+            date += datetime.timedelta(1)
+        print("</body></html>", file=writer)
 
 
 if __name__ == '__main__':
