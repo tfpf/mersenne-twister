18ad34113704b31b124e363ba00eeca95521a904
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-07-26 22:51:10 +0530

    Add separating lines

--- examples/sudoku/multigen.py
+++ examples/sudoku/multigen.py
@@ -13,14 +13,19 @@ def main():
     with open("index.html", "w", encoding="utf8") as writer:
         print("<!DOCTYPE html><html><head><title>Sudoku</title></head><body>", file=writer)
         date = datetime.date.today()
-        for _ in range(31):
+        for _ in range(num_of_puzzles):
             print("<h1>", date.strftime("%A %d %B %Y"), "</h1>", file=writer, sep="")
-            for icon, difficulty in zip(["✍️", "✍️✍️"], ["11", "12"]):
-                print("<h2>", icon, "</h2><pre>", file=writer, flush=True, sep="")
+            for idx, difficulty in enumerate(["11", "12"], 1):
+                print("<h2>", "✍️" * idx, "</h2><pre>", file=writer, flush=True, sep="")
                 subprocess.check_call(["./sudoku", difficulty], stdout=writer)
                 print("</pre>", file=writer)
             date += datetime.timedelta(1)
         print("</body></html>", file=writer)
+
+    with open("index.html", "r", encoding="utf8") as reader:
+        contents = reader.read()
+    with open("index.html", "w", encoding="utf8") as writer:
+        print(contents.replace("   ", " ┃ ").replace("\n\n", "\n  ━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"), file=writer)
 
 
 if __name__ == '__main__':
