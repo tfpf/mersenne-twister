b3fafb542dbad0dcb9a79f8e369183ae4ca1741b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-10-19 13:55:41 +0530

    Scroll to today's sudoku puzzle

--- examples/sudoku/multigen.py
+++ examples/sudoku/multigen.py
@@ -9,6 +9,14 @@ html_begin = """
 <html>
   <head>
     <title>Sudoku</title>
+    <script>
+        window.addEventListener("load", () => {
+            const today = new Date();
+            const headerId = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+            const headerElement = document.getElementById(headerId);
+            headerElement.scrollIntoView({behavior: "smooth", block: "start"});
+        });
+    </script>
     <style type="text/css">
       td {border: 1px solid; height: 30px; text-align: center; width: 30px;}
       td.even {background-image: linear-gradient(to bottom right, #D0E0E8, #FFFFFF)}
@@ -23,6 +31,7 @@ html_end = """
 </html>
 """
 
+
 def main():
     try:
         num_of_puzzles = int(sys.argv[1])
@@ -31,9 +40,23 @@ def main():
 
     with open("index.html", "w", encoding="utf8") as writer:
         print(html_begin, file=writer)
-        date = datetime.date.today()
+        date = datetime.date.today()  # noqa: DTZ011
         for _ in range(num_of_puzzles):
-            print("    <h1>", date.strftime("%A %d %B %Y"), "</h1>", file=writer, sep="")
+            # JavaScript does not have a built-in method to write local dates
+            # in the ISO 8601 format, so write the numbers manually.
+            print(
+                '    <h1 id="',
+                date.year,
+                "-",
+                date.month,
+                "-",
+                date.day,
+                '">',
+                date.strftime("%A %d %B %Y"),
+                "</h1>",
+                file=writer,
+                sep="",
+            )
             for idx, difficulty in enumerate(["11", "12"], 1):
                 print("    <h2>", "✍️" * idx, "</h2>", file=writer, flush=True, sep="")
                 subprocess.check_call(["./sudoku", difficulty, "dummy argument to get HTML output"], stdout=writer)
@@ -41,5 +64,5 @@ def main():
         print(html_end, file=writer)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
